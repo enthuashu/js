b81d8467bc06130a696d9ae3eec25f9427e8db59
Ashutosh Mishra <enthuashu@gmail.com>
2022-12-03 22:40:00 +0530

    day 2

---
+++ letNconst.js
@@ -0,0 +1,48 @@
+// Variables are containers which are used to store data (Bucket for storing rice)
+
+// Varibles are initalized with two types of declarations
+// 1. let => let is used when data needs to be updated in future
+// 2. const => when data have to kept constant
+// In other langauges declarations depends on type of data we are using
+// for e.g. In c++, we use int for storing integer, string for storing string values
+// bool for storing boolean
+// but in Javscript you can use same declaration(let/const) for every data type
+
+// Use of Let (when data will be updated)
+// let a = 10;
+// a = 15;
+// console.log(a);
+
+// Use of const (when data have to be constant)
+// const a = 10;
+// a = 15;
+// console.log(a);
+
+// in other languages, declration depend on data type
+// int a = 10;
+// string a = "ashu";
+// boolean a = true;
+
+// In JS, we can use let/const for each kind of data
+let a = 10; // number
+let b = "ashu"; // string
+let c = true; // boolean
+let d = [1, 3, 4, 5]; //array
+let e = { a: "ashu" }; // object
+console.log("let declrations for each data type");
+console.log(a, "number");
+console.log(b, "string");
+console.log(c, "boolean");
+console.log(d, "array");
+console.log(e, "object");
+console.log("Const declrations for each data type");
+const f = 10; // number
+const g = "ashu"; // string
+const h = true; // boolean
+const i = [1, 3, 4, 5]; //array
+const j = { a: "ashu" }; // object
+console.log(f, "number");
+console.log(g, "string");
+console.log(h, "boolean");
+console.log(i, "array");
+console.log(j, "object");
