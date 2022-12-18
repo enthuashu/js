2a35be178bf26cbb54a0535a13ad39d72cf9b26d
Ashutosh Mishra <enthuashu@gmail.com>
2022-12-18 22:54:29 +0530

    for of loop, mapping and object

--- ForOfLoop.js
+++ ForOfLoop.js
@@ -6,7 +6,9 @@
 //     console.log(arr[i]);
 // }
 
-const arr = [
+// For of loop is similar as, array mapping, array find
+
+const practicearr = [
   "Element 1",
   "Element 2",
   "Element 3",
@@ -16,11 +18,23 @@ const arr = [
   "Element 7",
 ];
 // a similar kind of short for of loop can be achieved to do the same task
-for (let candidate of arr) {
-  if (candidate === "Element 3") {
-    continue;
-  }
-  console.log(candidate);
-}
+// for (let candidate of practicearr) {
+//   console.log(candidate, " of for of loop");
+// }
 // The variable a starts from first element of array and goes to last
 // by updating its value to next element one by one
+
+// array mapping
+// arrayname.map((candidate)=>{
+
+// })
+// practicearr.map((candidate) => {
+//   console.log(candidate, " of mapping");
+// });
+
+// array find
+// const findedelement = practicearr.find((candidate) => {
+//   return candidate === "Element 4";
+// });
+
+// console.log(findedelement);
---
+++ object.js
@@ -0,0 +1,38 @@
+const tutorialcopy10rs = {
+  name: "Ashutosh Mishra",
+  branch: "ECE",
+  rollno: "2019041035",
+  session: "2019-2023",
+  "college name": "MMMUT,GKP",
+  age: 10,
+};
+
+// console.log(tutorialcopy10rs["name"]);
+// console.log(tutorialcopy10rs["branch"]);
+// console.log(tutorialcopy10rs["rollno"]);
+// console.log(tutorialcopy10rs["session"]);
+// console.log(tutorialcopy10rs["college"]);
+
+// Whenever we want to store data which is in the form of key value pairs, then
+// Object is used
+//Keys should never have
+//  spaces between them and
+//  should not start with any number or special characters
+// if it has spaces, then key should be enclosed within parenthesis "" (string form)
+// console.log(tutorialcopy10rs);
+
+//accessing values of object  in two ways
+// firstway
+// console.log(tutorialcopy10rs["name"]);
+// console.log(tutorialcopy10rs["branch"]);
+// console.log(tutorialcopy10rs["rollno"]);
+// console.log(tutorialcopy10rs["session"]);
+// console.log(tutorialcopy10rs["college"]);
+
+// console.log(tutorialcopy10rs.name);
+// console.log(tutorialcopy10rs.branch);
+// console.log(tutorialcopy10rs["college name"]);
+
+// to get all the keys of an object
+// const objectkeys = Object.keys(tutorialcopy10rs);
+// console.log(objectkeys);
